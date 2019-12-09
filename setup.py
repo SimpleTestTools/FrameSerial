8007614a1ccf1c7825faf06ac926ec80aff877e5
John Ott <john.ott@johnott.us>
2019-12-09 11:54:11 -0500

    Add basic python layout

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="frame-serial",
+    url="https://github.com/SimpleTestTools/FrameSerial",
+    package_dir={"": "py"},
+    packages=find_packages(where="py"),
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
+    install_requires=["pyserial"],
+    extras_require={"dev": ["black"],},
+)
